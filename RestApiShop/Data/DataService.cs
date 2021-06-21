2e8b7dc289c9cb7b130e079c9e2935d4156c5832
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-06-21 16:01:47 +0300

    DataService for models added

---
+++ RestApiShop/Data/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestApiShop.Models;
+
+namespace RestApiShop.Data
+{
+    public class DataService
+    {
+        public List<Crockery> CrockeryPieces { get; set; }
+        public List<Fruit> Fruits { get; set; }
+        public List<Vegetable> Vegetables { get; set; }
+
+        public DataService(List<Crockery> crockeryPieces, List<Fruit> fruits, List<Vegetable> vegetables)
+        {
+            CrockeryPieces = crockeryPieces;
+            Fruits = fruits;
+            Vegetables = vegetables;
+        }
+    }
+}
