f9dc8afeb62ff598ad4498ff7b9a91ec1ced0c11
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-06-22 19:06:52 +0300

    Made the Entity model an Abstract class

--- RestApiShop/Models/Base/Entity.cs
+++ RestApiShop/Models/Base/Entity.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RestApiShop.Models.Base
 {
-    public class Entity
+    public abstract class Entity
     {
         public int Id { get; set; }
         public string Name { get; set; }
