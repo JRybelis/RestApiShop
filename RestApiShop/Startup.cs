81b6a253dc9ec09f6b7183a5de0e1ee9b50cacc3
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-06-22 19:07:59 +0300

    Registered DataService.cs

--- RestApiShop/Startup.cs
+++ RestApiShop/Startup.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using RestApiShop.Data;
 
 namespace RestApiShop
 {
@@ -26,7 +27,7 @@ namespace RestApiShop
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddSingleton<DataService>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
