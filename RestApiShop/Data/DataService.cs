58a0177d7194ccf417cc48ed274861a4a18f2530
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-06-28 15:14:23 +0300

    refactored dataService props with instantialisation

--- RestApiShop/Data/DataService.cs
+++ RestApiShop/Data/DataService.cs
@@ -5,15 +5,8 @@ namespace RestApiShop.Data
 {
     public class DataService
     {
-        public List<Crockery> CrockeryPieces { get; set; }
-        public List<Fruit> Fruits { get; set; }
-        public List<Vegetable> Vegetables { get; set; }
-
-        public DataService()
-        {
-            CrockeryPieces = new List<Crockery>();
-            Fruits = new List<Fruit>();
-            Vegetables = new List<Vegetable>();
-        }
+        public List<Crockery> CrockeryPieces { get; set; } = new();
+        public List<Fruit> Fruits { get; set; } = new();
+        public List<Vegetable> Vegetables { get; set; } = new();
     }
 }
