1beadd628d699635df6a7ab5644497f1ef972650
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-07-04 15:20:30 +0300

    fluent API onModelCreation methods + new table for Price added

--- RestApiShop/Migrations/20210704121709_PriceFluentApi.Designer.cs
+++ RestApiShop/Migrations/20210704121709_PriceFluentApi.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RestApiShop.Data;
 
 namespace RestApiShop.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20210704121709_PriceFluentApi")]
+    partial class PriceFluentApi
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RestApiShop/Migrations/20210704121709_PriceFluentApi.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestApiShop.Migrations
+{
+    public partial class PriceFluentApi : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Vegetables",
+                type: "nvarchar(51)",
+                maxLength: 51,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(51)",
+                oldMaxLength: 51,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Vegetables",
+                type: "decimal(9,2)",
+                precision: 9,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Shops",
+                type: "nvarchar(51)",
+                maxLength: 51,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(51)",
+                oldMaxLength: 51,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Fruits",
+                type: "nvarchar(51)",
+                maxLength: 51,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(51)",
+                oldMaxLength: 51,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Fruits",
+                type: "decimal(9,2)",
+                precision: 9,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "CrockeryItems",
+                type: "nvarchar(51)",
+                maxLength: 51,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(51)",
+                oldMaxLength: 51,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "CrockeryItems",
+                type: "decimal(9,2)",
+                precision: 9,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Vegetables");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Fruits");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "CrockeryItems");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Vegetables",
+                type: "nvarchar(51)",
+                maxLength: 51,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(51)",
+                oldMaxLength: 51);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Shops",
+                type: "nvarchar(51)",
+                maxLength: 51,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(51)",
+                oldMaxLength: 51);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Fruits",
+                type: "nvarchar(51)",
+                maxLength: 51,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(51)",
+                oldMaxLength: 51);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "CrockeryItems",
+                type: "nvarchar(51)",
+                maxLength: 51,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(51)",
+                oldMaxLength: 51);
+        }
+    }
+}
