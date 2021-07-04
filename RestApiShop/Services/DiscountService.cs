a84ceb9f6501a2276c669005be746e00c6a81ac9
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-07-04 15:40:48 +0300

    DiscountService created

---
+++ RestApiShop/Services/DiscountService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestApiShop.Services
+{
+    public class DiscountService
+    {
+        private const int DiscountPercentage = 10;
+
+        public decimal CalculateDiscount(decimal price)
+        {
+            return price / 100 * DiscountPercentage;
+        }
+    }
+}
