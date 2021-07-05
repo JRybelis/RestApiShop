5b63f4e0d20ce3cddfa734ab230f044daa58bcc8
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-07-05 11:59:58 +0300

    Updated MappingsProfile with ShopItemDto and Item mapping

--- RestApiShop/Mappings/MappingsProfile.cs
+++ RestApiShop/Mappings/MappingsProfile.cs
@@ -1,10 +1,12 @@
 ﻿using AutoMapper;
 using RestApiShop.Dtos;
+using RestApiShop.Dtos.Base;
 using RestApiShop.Dtos.Crockery;
 using RestApiShop.Dtos.Fruit;
 using RestApiShop.Dtos.Shop;
 using RestApiShop.Dtos.Vegetable;
 using RestApiShop.Entities;
+using RestApiShop.Entities.Base;
 
 namespace RestApiShop.Mappings
 {
@@ -15,6 +17,7 @@ namespace RestApiShop.Mappings
             CreateMap<CrockeryDto, Crockery>().ReverseMap();
             CreateMap<FruitDto, Fruit>().ReverseMap();
             CreateMap<VegetableDto, Vegetable>().ReverseMap();
+            CreateMap<ShopItemDto, Item>().ReverseMap();
             CreateMap<ShopDto, Shop>().ReverseMap();
         }
     }
