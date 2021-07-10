6a5f98469c92e435a24d101dd8d13a88079e3261
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-07-11 00:24:56 +0300

    created a buyItemDto and added Buy method to the VegetableController

--- RestApiShop/Controllers/VegetableController.cs
+++ RestApiShop/Controllers/VegetableController.cs
@@ -3,7 +3,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using RestApiShop.Controllers.Base;
+using RestApiShop.Data;
+using RestApiShop.Dtos.Base;
 using RestApiShop.Dtos.Vegetable;
 using RestApiShop.Entities.ShopItems;
 using RestApiShop.Repositories;
@@ -13,27 +14,58 @@ namespace RestApiShop.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class VegetableController : GenericControllerBase<VegetableDto, Vegetable>
+    public class VegetableController : ControllerBase
     {
         private readonly IMapper _mapper;
+        private DataContext _context;
         private readonly IGenericRepository<Vegetable> _repository;
         private readonly PriceCalculationService<VegetableDto> _priceCalculation;
 
-        public VegetableController(IMapper mapper, IGenericRepository<Vegetable> repository, PriceCalculationService<VegetableDto> priceCalculation) : base(mapper, repository)
+        public VegetableController(IMapper mapper, IGenericRepository<Vegetable> repository, PriceCalculationService<VegetableDto> priceCalculation, DataContext context)
         {
             _mapper = mapper;
             _repository = repository;
             _priceCalculation = priceCalculation;
+            _context = context;
         }
 
         [HttpGet]
-        public override async Task<IEnumerable<VegetableDto>> GetAll()
+        public async Task<IEnumerable<VegetableDto>> GetAll()
         {
             var entities = await _repository.GetAll();
             var dtos = _mapper.Map<IEnumerable<VegetableDto>>(entities);
             var updatedDtos = dtos.Select(d => _priceCalculation.ApplyDiscount(d));
 
             return updatedDtos;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<VegetableDto> GetById(int id)
+        {
+            var entity = await _repository.GetById(id);
+            return _mapper.Map<VegetableDto>(entity);
+        }
+
+        [HttpPost]
+        public async Task Upsert(VegetableDto dto)
+        {
+            var entity = _mapper.Map<Vegetable>(dto);
+            await _repository.Upsert(entity);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            await _repository.Delete(id);
+        }
+
+        [HttpPost("/buy")]
+        public decimal Buy(BuyItemDto buyItemDto)
+        {
+            var item = _context.Vegetables.FirstOrDefault(v => v.Name == buyItemDto.ItemName);
+            var totalPrice = item.Price * buyItemDto.Quantity;
+
+            return totalPrice;
         }
     }
 
---
+++ RestApiShop/Dtos/Base/BuyItemDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestApiShop.Dtos.Base
+{
+    public class BuyItemDto
+    {
+        public string ItemName { get; set; }
+        public int Quantity { get; set; }
+    }
+}
