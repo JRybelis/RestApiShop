5f6b0655ab6e53671016c95f279f41ff6d183bf2
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-07-15 20:43:29 +0300

    Created IntegrationTests

---
+++ EfCoreUnitTests/IntegrationTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Newtonsoft.Json;
+using RestApiShop;
+using RestApiShop.Entities.ShopItems;
+using Xunit;
+
+namespace EfCoreUnitTests
+{
+    public class IntegrationTests
+    {
+        private readonly CustomWebApplicationFactory<Startup> _factory = new CustomWebApplicationFactory<Startup>();
+
+        [Fact]
+        public async Task IntegrationTest()
+        {
+            var client = _factory.CreateClient();
+            var result = await client.GetAsync("/Vegetable");
+            
+            var stringContent = await result.Content.ReadAsStringAsync();
+            var vegetables = JsonConvert.DeserializeObject<List<Vegetable>>(stringContent);
+
+            result.EnsureSuccessStatusCode();
+            vegetables.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public async Task Delete_IntegrationTest()
+        {
+            var client = _factory.CreateClient();
+            await client.DeleteAsync("/Vegetable/1");
+            
+            var result = await client.GetAsync("/Vegetable");
+            var stringContent = await result.Content.ReadAsStringAsync();
+
+            var vegetables = JsonConvert.DeserializeObject<List<Vegetable>>(stringContent);    
+            
+            result.EnsureSuccessStatusCode();
+            vegetables.Should().BeEmpty();
+        }
+        
+    }
+}
