2c3f223e4898d6944b55f8949309e41e4c462459
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-07-15 20:42:11 +0300

    Created a data seeder for Vegetable entity in tests.

---
+++ EfCoreUnitTests/DataSeeder.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using RestApiShop.Data;
+using RestApiShop.Entities.ShopItems;
+
+namespace EfCoreUnitTests
+{
+    public static class DataSeeder
+    {
+        public static void SeedData(DataContext context)
+        {
+            if (!context.Vegetables.Any())
+            {
+                context.Vegetables.Add(new Vegetable()
+                {
+                    Name = "Default"
+                });
+
+                context.SaveChangesAsync();
+            }
+        }
+    }
+}
